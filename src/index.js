fafaceb1577e84106ac535a240a123e722c9d0e1
gergokutu <gergo.kutu@gmail.com>
2020-04-22 15:52:25 +0200

    Show previous steps list on the screen

--- src/index.js
+++ src/index.js
@@ -163,8 +163,24 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares)
-    let status;
 
+    // Map 2nd arg is the index
+    // move > index
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        `Go to move #${move}` :
+        'Go to game start'
+      
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      );
+    });
+
+    let status;
     if (winner) {
       status = `Winner ${winner}`;
     } else {
@@ -183,7 +199,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
