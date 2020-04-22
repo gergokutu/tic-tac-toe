c3ac411e27872fe4c87a3c1e88d38aa4cf438a8d
gergokutu <gergo.kutu@gmail.com>
2020-04-22 13:32:46 +0200

    Update handleClick

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ import './index.css';
 // Do not forget to pass props!
 function Square(props) {
   return (
-    <button className="square" onClick={ props.onClick }>
-      { props.value }
+    <button className="square" onClick={props.onClick}>
+      {props.value}
     </button>
   )
 }
@@ -45,14 +45,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true,
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   } 
 
   renderSquare(i) {
@@ -71,19 +74,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          { this.renderSquare(0) }
-          { this.renderSquare(1) }
-          { this.renderSquare(2) }
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          { this.renderSquare(3) }
-          { this.renderSquare(4) }
-          { this.renderSquare(5) }
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          { this.renderSquare(6) }
-          { this.renderSquare(7) }
-          { this.renderSquare(8) }
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
